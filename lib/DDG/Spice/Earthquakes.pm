003e49070a807eb69d5cbadf633123ecb408201a
Dylan Lloyd <dylan@dylansserver.com>
2012-10-25 14:09:43 -0400

    added 1hr cache time as per SEISMI data update

--- lib/DDG/Spice/Earthquakes.pm
+++ lib/DDG/Spice/Earthquakes.pm
@@ -10,6 +10,8 @@ triggers any => "earthquake", "earthquakes";
 spice to => 'http://www.seismi.org/api/$1/$2?limit=50';
 spice from => '([^/]+)/([^/]+)';
 
+spice proxy_cache_valid => "200 1hr";
+
 spice wrap_jsonp_callback => 1;
 
 handle query_lc => sub {
