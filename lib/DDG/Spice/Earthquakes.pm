8ec3d9ad650eb43b68aefed8d633097682fffa82
Dylan Lloyd <dylan@dylansserver.com>
2012-10-29 16:38:12 -0400

    omg

--- lib/DDG/Spice/Earthquakes.pm
+++ lib/DDG/Spice/Earthquakes.pm
@@ -10,7 +10,7 @@ triggers any => "earthquake", "earthquakes";
 spice to => 'http://www.seismi.org/api/$1/$2?limit=50';
 spice from => '([^/]+)(?:/([^/]+))?';
 
-spice proxy_cache_valid => "1hr";
+spice proxy_cache_valid => "1h";
 
 spice wrap_jsonp_callback => 1;
 
