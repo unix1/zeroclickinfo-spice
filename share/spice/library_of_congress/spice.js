0bd01b54ece1172778629e15db4998a57d121eaa
Dylan Lloyd <dylan@dylansserver.com>
2013-01-27 10:39:00 -0500

    add the query to the titile

--- share/spice/library_of_congress/spice.js
+++ share/spice/library_of_congress/spice.js
@@ -1,4 +1,7 @@
 function ddg_spice_library_of_congress(response) {
+
+    var query = decodeURIComponent(rq);
+    query = query.replace(/^ ?(library of congress) ?/gi, "")
     
     var parseXML;
 
@@ -30,7 +33,7 @@ function ddg_spice_library_of_congress(response) {
 	var items = new Array();
 	items[0] = new Array();
     items[0]['a'] = 'test';
-	items[0]['h'] = " (Library of Congress)";
+	items[0]['h'] = query + " (Library of Congress)";
 	items[0]['s'] = 'Maps';
 	items[0]['u'] = 'http://loc.gov';
     items[0]["force_big_header"] = true;
