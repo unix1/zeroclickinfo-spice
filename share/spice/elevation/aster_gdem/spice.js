6e9dec73c4d1236577427adfb5c5a2d960f43572
Dylan Lloyd <dylan@dylansserver.com>
2013-01-30 20:18:48 -0500

    get results of all data sources on each call with nrj

--- share/spice/elevation/aster_gdem/spice.js
+++ share/spice/elevation/aster_gdem/spice.js
@@ -1,16 +1,46 @@
+var items = new Array();
+items[0] = new Array();
+items[0]['a'] = '';
+items[0]['s'] = 'GeoNames';
+items[0]['u'] = 'http://www.geonames.org/';
+nra(items);
+
+// Ocean areas have been masked as "no data"
+// and have been assigned a value of -9999 
+
 function ddg_spice_elevation_aster_gdem(r) {
-    // Ocean areas have been masked as "no data"
-    // and have been assigned a value of -9999 
     if (r.astergdem == -9999) return;
     var answer = 'Elevation at ' + r.lat + '° latitude, '
-               + r.lng + '° longitude is ' + r.astergdem + 'm.'
-               + ' Measured by the <a href="https://en.wikipedia.org/wiki/'
+               + r.lng + '° longitude is:'
+               + '<ul>'
+               + '<li>' + r.astergdem + 'm'
+               + ' by the <a href="https://en.wikipedia.org/wiki/'
                + 'ASTER_GDEM#ASTER_Global_Digital_Elevation_Model">'
-               + 'ASTER Global Digital Elevation Model</a>.';
-	var items = new Array();
-	items[0] = new Array();
+               + 'ASTER Global Digital Elevation Model</a>'
+               + '</li>';
     items[0]['a'] = answer;
-	items[0]['s'] = 'GeoNames';
-	items[0]['u'] = 'http://www.geonames.org/';
+
+    nrj('/js/spice/elevation/srtm3/' + r.lat + '/' + r.lng);
+}
+function ddg_spice_elevation_srtm3(r) {
+    if (r.srtm3 == -32768) return;
+    var answer = '<li>'
+               + r.srtm3 + 'm'
+               + ' by the <a href="https://en.wikipedia.org/wiki/SRTM">'
+               + 'Shuttle Radar Topography Mission</a>'
+               + '</li>';
+    items[0]['a'] += answer;
+
+    nrj('/js/spice/elevation/gtopo30/' + r.lat + '/' + r.lng);
+}
+function ddg_spice_elevation_gtopo30(r) {
+    if (r.gtopo30 == -32768) return;
+    var answer = '<li>'
+               + r.gtopo30 + 'm'
+               + ' by the <a href="https://en.wikipedia.org/wiki/GTOPO30">'
+               + 'GTOPO30 Digital Elevation Model</a>'
+               + '</li>'
+               + '</ul>';
+    items[0]['a'] += answer;
 	nra(items);
 }
