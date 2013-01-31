3db9b82ee2fc20106f634e9aa99fdd2b2ba1c665
Dylan Lloyd <dylan@dylansserver.com>
2013-01-30 22:05:56 -0500

    give relative dates for each earthquake

--- share/spice/earthquakes/spice.js
+++ share/spice/earthquakes/spice.js
@@ -1,4 +1,5 @@
 function ddg_spice_earthquakes(response) {
+    console.log(response);
     var query = DDG.get_query();
     var year = query.search(/\d{4}/);
     query = (year == -1 ? 'Recent Earthquakes'
@@ -9,12 +10,22 @@ function ddg_spice_earthquakes(response) {
         var earthquake = earthquakes[i];
         var region = earthquake.region.charAt(0).toUpperCase()
                    + earthquake.region.slice(1);
+        var date = new Date(earthquake.timedate.replace(/-/g, '/'));
+        var time = Math.abs(date.getHours() - 12) + ':' + date.getMinutes()
+                 + (date.getHours() < 12 ? 'am' : 'pm');
+        date.setHours(0,0,0,0);
+        var now = new Date().setHours(0,0,0,0);
+        var time_difference = now - date.getTime();
+        var days_ago = Math.floor(time_difference / (24*60*60*1000));
+        var relative_time = (days_ago == 0 ? 'today' :
+                                (days_ago == 1 ? 'yesterday' :
+                                     days_ago + ' days ago'));
 		out += '<fieldset>'
              + '<legend>'
              + '<a href="http://maps.google.com/?ll='
              + earthquake.lat + ',' + earthquake.lon + '">'
-             + region + '</a>'
-             + ' at ' + earthquake.timedate.replace(/-/g,'/')
+             + earthquake.region + '</a> '
+             + relative_time + ' at ' + time
              + '</legend>'
              + + earthquake.magnitude + ' magnitude at '
              + earthquake.depth + 'km depth '
