2353c29f17b9695314ba4de4e627fb01bb822c2f
Dylan Lloyd <dylan@dylansserver.com>
2013-02-03 15:24:49 -0500

    started bitcoin spice
    uservoice request:
    https://duckduckhack.uservoice.com/forums/5168-plugins/suggestions/3593146-zero-click-info-for-bitcoin-addresses-

---
+++ lib/DDG/Spice/Bitcoin.pm
@@ -0,0 +1,19 @@
+package DDG::Spice::Bitcoin;
+
+use DDG::Spice;
+
+attribution web => ['http://dylansserver.com','Dylan Lloyd'],
+            email => ['dylan@dylansserver.com','Dylan Lloyd'];
+
+triggers any => 'bitcoin', 'bitcoins', 'btc';
+
+spice to => 'http://blockchain.info/address/$1?format=json';
+
+spice wrap_jsonp_callback => 1;
+
+handle remainder => sub {
+    s/ ?address( of)? ?//;
+    return $_;
+};
+
+1;
---
+++ share/spice/bitcoin/spice.js
@@ -0,0 +1,15 @@
+function ddg_spice_bitcoin(response) {
+    console.log(response);
+    var query = decodeURIComponent(rq);
+    query = query.replace(/(btc|bitcoins?)( address( of)?)?/gi, "")
+
+	var items = new Array();
+	items[0] = new Array();
+    items[0]['a'] = 'test';
+	items[0]['h'] = query + " (Bitcoin Address)";
+	items[0]['s'] = 'BitCoin.info';
+	items[0]['u'] = 'http://bitcoin.info';
+    items[0]["force_big_header"] = true;
+	
+	nra(items);
+}
