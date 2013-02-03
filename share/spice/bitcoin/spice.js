d971b0081afe6165b007624c750752004ae9fc37
Dylan Lloyd <dylan@dylansserver.com>
2013-02-03 18:17:57 -0500

    added balance data and address tag where possible

--- share/spice/bitcoin/spice.js
+++ share/spice/bitcoin/spice.js
@@ -3,9 +3,26 @@ function ddg_spice_bitcoin(response) {
     var query = decodeURIComponent(rq);
     query = query.replace(/(btc|bitcoins?)( address( of)?)?/gi, "")
 
+    var balance = '' + response.final_balance/1000;
+    balance = balance.replace(/(\d{3})/g, '$1,').replace(/,?\.\d*|,$/g, '');
+
+    res = response;
+
+    var address_tag = '';
+    response.txs.map(
+        function (tx) {
+            if (tx.out[0].addr_tag != undefined)
+                address_tag = tx.out[0].addr_tag;
+        }
+    );
+
+    var answer = (address_tag != '' ? '<h1>' + address_tag + '</h1><br>' : '')
+               + 'Total balance: ' + balance + '฿<br>'
+               + 'Total transactions: ' + response.n_tx + '<br>';
+
 	var items = new Array();
 	items[0] = new Array();
-    items[0]['a'] = 'test';
+    items[0]['a'] = answer;
 	items[0]['h'] = query + " (Bitcoin Address)";
 	items[0]['s'] = 'BitCoin.info';
 	items[0]['u'] = 'http://bitcoin.info/address/' + query;
