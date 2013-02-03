f1f3d4830eb98aca1e02644324eae1a442b8a0b1
Dylan Lloyd <dylan@dylansserver.com>
2013-02-03 15:32:20 -0500

    use appropriate source link with address hash

--- share/spice/bitcoin/spice.js
+++ share/spice/bitcoin/spice.js
@@ -8,7 +8,7 @@ function ddg_spice_bitcoin(response) {
     items[0]['a'] = 'test';
 	items[0]['h'] = query + " (Bitcoin Address)";
 	items[0]['s'] = 'BitCoin.info';
-	items[0]['u'] = 'http://bitcoin.info';
+	items[0]['u'] = 'http://bitcoin.info/address/' + query;
     items[0]["force_big_header"] = true;
 	
 	nra(items);
