7d8386dff301f0e58aed4b956bcfaac23dcc72ad
Caine Tighe <caine@duckduckgo.com>
2013-03-13 17:11:44 -0400

    DDG::Spice::News::Site: handle slow load of legacy js more gracefully

--- share/spice/news/site/spice.js
+++ share/spice/news/site/spice.js
@@ -2,10 +2,11 @@
 nrj('/js/nrty.js');
 
 function ddg_spice_news_site(stories) {
+    var GOD_SPEED = 50;
     window.setTimeout(function () {
-	    if (news.nrty)
+	    if (typeof(news) != "undefined")
 		news.nrty(stories, 1);
 	    else
 		console.log("DDG::Spice::News::Site: unable to load legacy news JS fast enough");
-	}, 9);
+	}, GOD_SPEED);
 }
