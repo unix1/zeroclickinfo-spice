41e0461704eb7d19e0602ff33ca784265107890a
Torsten Raudssus <torsten@raudss.us>
2013-04-25 19:46:45 +0200

    Added travis and PodWeaver

---
+++ share/spice/news_site/spice.js
@@ -0,0 +1,9 @@
+function ddg_spice_news_site(stories) {
+
+    // Reuse the old method and add a seatbelt...
+    nrj('/js/nrty.js');
+
+    if (typeof(news.nrty) === "function") {
+	news.nrty(stories);
+    }
+}
